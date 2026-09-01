9ddd58ec41917b5b01a2b1dc773da5c812788f76
Nathan Lazo <nathanlazo@outlook.com>
2026-09-01 12:56:09 -0600

    fix: update sidebar class names for improved styling

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -214,7 +214,7 @@ function Sidebar({
 
   return (
     <div
-      className="group peer hidden text-sidebar-foreground md:block"
+      className="group peer hidden md:block"
       data-state={state}
       data-collapsible={state === "collapsed" ? collapsible : ""}
       data-variant={variant}
@@ -236,7 +236,7 @@ function Sidebar({
       <div
         data-slot="sidebar-container"
         className={cn(
-          "fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 [transition-timing-function:cubic-bezier(0.2,0,0,1)] md:flex",
+          "fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) text-sidebar-foreground transition-[left,right,width] duration-200 [transition-timing-function:cubic-bezier(0.2,0,0,1)] md:flex",
           side === "left"
             ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
